18930ab52bdc370ea4cdb060a53e9651fe19741b
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-07-19 23:32:18 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+aDictPalavras   = {
+    'Frutas': ['Laranja', 'Maçã', 'Abacate', 'Abacaxi', 'Banana', 'Tomate'],
+    'Pais': ['Brasil', 'Canada', 'Estados Unidos', 'Portugal', 'Japão'],
+    'Objeto': ['Colher', 'Ventilador', 'Televisão', 'Celular', 'Rádio', 'Computador']
+}
+cPalavra        = ''
+cLetra          = ''
+
+print(aDictPalavras['Frutas'])
