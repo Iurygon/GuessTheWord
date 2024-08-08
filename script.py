7f32f67f7d7be29bdd830697d9722cc65c0ce8c8
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-08-07 23:52:34 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,18 +1,13 @@
 from random import randrange
 from time import sleep
+import os
 
 aObjeto         = ['COLHER', 'VENTILADOR', 'TRAVESSEIRO', 'CELULAR', 'COMPUTADOR']
 aPais           = ['BRAZIL', 'CANADA', 'MONGOLIA', 'PORTUGAL', 'CHILE']
 aFrutas         = ['LARANJA', 'ABACATE', 'ABACAXI', 'BANANA', 'TOMATE']
 aTemas          = [aObjeto, aPais, aFrutas]
 
-nTentativa      = 5
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
-
-print('Bem vindo ao jogo da forca! Você terá cinco tentativas de advinhar a palavra escolhida.')
-sleep(1.5)
-cTemaEscolhido  = input('Selecione um tema de palavra: [0] Objetos [1] Países [2] Frutas: ').strip(' ')
-sleep(1.5)
 
 def palavraAleatoria(tema):
     cPalavra = tema[randrange(0, len(tema), 1)]
@@ -25,24 +20,44 @@ def encontraLetra(letraEscolhida, palavra, resultado):
     return resultado
 
 def jogoDaForca():
-    match cTemaEscolhido:
-        case '0': cPalavra = palavraAleatoria(aObjeto)
-        case '1': cPalavra = palavraAleatoria(aPais)
-        case '2': cPalavra = palavraAleatoria(aFrutas)
-    print(f'Sua palavra tem {len(cPalavra)} caracteres.')
-    cResultado = []
-    for i in cPalavra:
-        cResultado.append('_')
-    for i in range(0, 5):
-        print(f'Esta é a tentativa número {i + 1}. Restam {5 - i}')
-        cLetra = input('Advinhe uma letra:')
-        cResultado = encontraLetra(cLetra, cPalavra, cResultado)
-        print(''.join(cResultado))
-    print('Acabaram as tentativas!')
-    cResposta = input('E então, qual é a palavra?').strip(' ').upper()
-    if cResposta == cPalavra:
-        print('Parabéns! Você acertou!!')
-    else:
-        print(f'Que pena, você não acertou! A palavra correta era {cPalavra}')
+    try:
+        cTemaEscolhido  = int(input('Selecione um tema de palavra: [0] Objetos [1] Países [2] Frutas: '))
+        match cTemaEscolhido:
+            case 0: cPalavra = palavraAleatoria(aObjeto)
+            case 1: cPalavra = palavraAleatoria(aPais)
+            case 2: cPalavra = palavraAleatoria(aFrutas)
+        print(f'Sua palavra tem {len(cPalavra)} caracteres.')
+        
+        cResultado = []
+        nPontuacao = 5
+        for i in cPalavra:
+            cResultado.append('_')
 
+        for i in range(0, 5):
+            print(f'Restam {5 - i} tentivas.')
+            cLetra = input('Advinhe uma letra:')
+            cResultado = encontraLetra(cLetra, cPalavra, cResultado)
+            print(''.join(cResultado))
+            nPalpite = int(input(f'Deseja realizar um palpite?  [0] Sim [1] Não. Você perderá suas chances restantes, mas ganhará {nPontuacao} pontos.'))
+            if nPalpite == 0:
+                break
+            nPontuacao = nPontuacao - 1
+
+        cResposta = input('E então, qual é a palavra?').strip(' ').upper()
+        if cResposta == cPalavra:
+            print(f'Parabéns! Você acertou e ganhou {nPontuacao} pontos!!')
+        else:
+            print(f'Que pena, você não acertou! A palavra correta era {cPalavra}')
+    except:
+        print('O valor digitado deve estar entre as opções disponíveis.')
+        jogoDaForca()
+#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+
+print('Bem vindo ao jogo da forca! Você terá cinco tentativas de advinhar a palavra escolhida.')
+sleep(1.5)
+# try:
+#     jogoDaForca()
+# except:
+#     print('O valor digitado deve estar entre as opções disponíveis.')
+    
 jogoDaForca()
